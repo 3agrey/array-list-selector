5b30e52807c3367231c1f23399d7c0570eda276a
3agrey <3ug3ag@gmail.com>
2012-02-05 04:34:13 +0200

    added solution

---
+++ arraylist/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace arraylist
+{
+    class Selector
+    {
+        static void Main(string[] args)
+        {
+            ArrayList counter = new ArrayList();
+            int remainder = 0;
+            int a = 0;
+            for (int ctr = 0; ctr < 100; ctr++)
+            {
+                counter.Add(ctr);
+            }
+            for (int ctr = 0; ctr < 100; ctr++)
+            {
+                a = Convert.ToInt32(counter[ctr]);
+                Math.DivRem(a, 2, out remainder);
+                if (remainder == 1)
+                    counter[ctr] = "";
+            }
+            PrintValues(counter);
+            Console.ReadKey();
+        }
+
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+                Console.Write(" {0}", obj);
+
+            Console.WriteLine();
+        }
+    }
+}
+
